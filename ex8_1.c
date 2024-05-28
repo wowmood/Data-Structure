790044c09a2b491a065247919aa7b11b82d0bd6c
Eun <127073538+eun-808@users.noreply.github.com>
2024-05-28 11:07:56 +0900

    순차배열 그래프 8-1

---
+++ ex8_1.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "adjMatrix.h"
+
+int main(void) {
+	int i;
+	graphType* G1 = (graphType*)malloc(sizeof(graphType));
+	graphType* G2 = (graphType*)malloc(sizeof(graphType));
+	graphType* G3 = (graphType*)malloc(sizeof(graphType));
+	graphType* G4 = (graphType*)malloc(sizeof(graphType));
+
+	graphType* G5 = (graphType*)malloc(sizeof(graphType));
+
+	//G1 구성 : 정점 u에 대한 간선 (u,v)의 삽입순서는 v가 큰 것부터.
+	createGraph(G1);
+	for (i = 0; i < 4; i++)
+		insertVertex(G1, i);	//G1의 정점 0~3 삽입
+	insertEdge(G1, 0, 3);
+	insertEdge(G1, 0, 1);
+	insertEdge(G1, 1, 3);
+	insertEdge(G1, 1, 2);
+	insertEdge(G1, 1, 0);
+	insertEdge(G1, 2, 3);
+	insertEdge(G1, 2, 1);
+	insertEdge(G1, 3, 2);
+	insertEdge(G1, 3, 1);
+	insertEdge(G1, 3, 0);
+
+	//G2 구성 : 정점 u에 대한 간선 (u,v)의 삽입순서는 v가 큰 것부터.
+	createGraph(G2);
+	for (i = 0; i < 3; i++)
+		insertVertex(G2, i);	// G2의 정점 0~2 삽입
+	insertEdge(G2, 0, 2);
+	insertEdge(G2, 0, 1);
+	insertEdge(G2, 1, 2);
+	insertEdge(G2, 1, 0);
+	insertEdge(G2, 2, 1);
+	insertEdge(G2, 2, 0);
+
+	//G3 구성 : 정점 u에 대한 간선 <u,v>의 삽입순서는 v가 큰 것부터.
+	createGraph(G3);
+	for (i = 0; i < 4; i++)
+		insertVertex(G3, i);	// G3의 정점 0~3 삽입
+	insertEdge(G3, 0, 3);
+	insertEdge(G3, 0, 1);
+	insertEdge(G3, 1, 3);
+	insertEdge(G3, 1, 2);
+	insertEdge(G3, 2, 3);
+
+	//G4 구성 : 정점 u에 대한 간선 <u,v>의 삽입순서는 v가 큰 것부터.
+	createGraph(G4);
+	for (i = 0; i < 3; i++)
+		insertVertex(G4, i);	// G4의 정점 0~2 삽입
+	insertEdge(G4, 0, 2);
+	insertEdge(G4, 0, 1);
+	insertEdge(G4, 1, 2);
+	insertEdge(G4, 1, 0);
+
+	createGraph(G5);
+	for (i = 0; i < 7; i++)
+		insertVertex(G5, i);	// G5의 정점 0~6 삽입 #7개 vertex의 개수
+	insertEdge(G5, 0, 1);
+	insertEdge(G5, 0, 2);
+	insertEdge(G5, 1, 0);
+	insertEdge(G5, 1, 3);
+	insertEdge(G5, 1, 4);
+	insertEdge(G5, 2, 0);
+	insertEdge(G5, 2, 4);
+	insertEdge(G5, 3, 1);
+	insertEdge(G5, 3, 6);
+	insertEdge(G5, 4, 1);
+	insertEdge(G5, 4, 2);
+	insertEdge(G5, 4, 6);
+	insertEdge(G5, 5, 6);
+	insertEdge(G5, 6, 3);
+	insertEdge(G5, 6, 4);
+	insertEdge(G5, 6, 5);
+
+
+
+	//--- 완성된 인접 행렬 출력
+	printf("\n G1의 인접 행렬");
+	print_adjMatrix(G1);
+
+	printf("\n\n G2의 인접 행렬");
+	print_adjMatrix(G2);
+
+	printf("\n\n G3의 인접 행렬");
+	print_adjMatrix(G3);
+
+	printf("\n\n G4의 인접 행렬");
+	print_adjMatrix(G4);
+
+	printf("\n\n G5의 인접 행렬");
+	print_adjMatrix(G5);
+
+	getchar();  return 0;
+}
